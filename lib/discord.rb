e55c0006d54b22bbbdb64907b6076e3124cf7b3c
Sean Culliton <smculliton@gmail.com>
2023-07-31 14:09:57 -0600

    add ruby version file

---
+++ lib/discord.rb
@@ -0,0 +1,21 @@
+require 'faraday'
+require 'dotenv'
+Dotenv.load
+
+class DiscordService
+  def self.conn
+    
+  end
+end
+
+
+
+
+{
+  "content": "Hello, World!",
+  "tts": false,
+  "embeds": [{
+    "title": "Hello, Embed!",
+    "description": "This is an embedded message."
+  }]
+}
