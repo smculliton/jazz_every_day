6fb7b1e0a4929a26a5717c7f97b5ae1299da1b56
Sean Culliton <smculliton@gmail.com>
2023-07-31 15:19:16 -0600

    add lambda_function file

---
+++ lambda_function.rb
@@ -0,0 +1,14 @@
+require './lib/post_generator'
+
+
+def lambda_handler(event:, context:)
+  twitter_keys = {
+    consumer_key: ENV["consumer_key"],
+    consumer_secret: ENV["consumer_secret"],
+    access_token: ENV["access_token"],
+    token_secret: ENV["token_secret"]
+  }
+  
+  pg = PostGenerator.new(twitter_keys)
+  pg.generate_post
+end
